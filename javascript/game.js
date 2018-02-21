877354867b52c736be1e691192d1c5be1e0c9bea
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-02-20 21:53:03 -0600

    Completes JS

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,8 @@
 // Wait for items to load fully
 $(document).ready(function () {
 
+    // Set up ========================================================
+    
     // Creating global variables
     var answer;
     var userTotal;
@@ -10,8 +12,20 @@ $(document).ready(function () {
     var fourthCrystal;
     var gamesWon;
     var gamesLost;
-    var message;
 
+    // Function to initalize game values at start
+    function initial () {
+
+        gamesWon = 0;
+
+        $('#gameWon').text(gamesWon);
+
+        gamesLost = 0;
+
+        $('#gameLost').text(gamesLost);
+
+    };
+    
     // Writing function to determine and assign random values
     function set () {
 
@@ -31,24 +45,78 @@ $(document).ready(function () {
         $('#crystalThree').val(thirdCrystal);
         $('#crystalFour').val(fourthCrystal);
 
-    }
+        // Displaying value of answer
+        $('#answer').text(answer);
 
-    // Run function above on load
+    };
+
+    // Function to be carried out if player correclty equals the given value
+    function gameWin (){
+
+        // Display 'You Won' in status
+        $('#status').text('You Won!');
+
+        // Add game to total games won
+        gamesWon++;
+
+        // Display new games won total
+        $('#gameWon').text(gamesWon);
+
+        // Reset values for answer and crystals
+        set ();
+
+    };
+
+     // Function to be carried out if player goes over given value
+     function gameLose (){
+
+        // Display 'You Won' in status
+        $('#status').text('You Lost!');
+
+        // Add game to total games won
+        gamesLost++;
+
+        // Display new games won total
+        $('#gameLost').text(gamesLost);
+
+        // Reset values for answer and crytals
+        set();
+
+    };
+
+    // Gameplay ======================================================
+
+    // Run initial and set functions on load
+
+    initial();
+
     set();
-
-    $('#answer').text(answer);
 
     // Assign value to each jewel
 
     // Listen for click 
     $('.crystal').on('click', function () {
 
-        console.log(this);
-    
         // On click determine value of crystal clicked and add to user total
-        userTotal = userTotal + $(this).val();
-        console.log(userTotal);
+        userTotal += parseInt($(this).val());
 
-    })
+        // Push new user total to answer
+
+        $('#userScore').text(userTotal);
+
+        // Check if user has correclty equaled the given value
+        if (userTotal === answer){
+
+            // Run function for game won
+            gameWin();
+
+        } else if (userTotal > answer){
+            
+            // Run function for game lost
+            gameLose();
+
+        }; 
+
+    });
 
 });
